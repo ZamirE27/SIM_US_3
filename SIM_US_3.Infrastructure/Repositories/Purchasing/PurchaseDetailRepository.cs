f84882442bacae1e098665b5d58797b3ba8062df
ZamirE27 <zaesco2001@gmail.com>
2025-11-12 00:18:57 -0500

    I'm still working on it

---
+++ SIM_US_3.Infrastructure/Repositories/Purchasing/PurchaseDetailRepository.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using SIM_US_3.Domain.Models;
+using SIM_US_3.Domain.Models.Interfaces;
+using SIM_US_3.Infrastructure.Common;
+using SIM_US_3.Infrastructure.Data;
+
+namespace SIM_US_3.Infrastructure.Repositories.Purchasing;
+
+public class PurchaseDetailRepository : Repository<PurchaseDetail>,IPurchaseDetailRepository
+{
+    public PurchaseDetailRepository(AppDbContext dbContext) : base(dbContext)
+    {
+    }
+
+    public async Task<IEnumerable<PurchaseDetail>> GetDetailsByPurchase(int purchaseId)
+    {
+        return await _dbset
+            .Where(x => x.PurchaseId == purchaseId)
+            .ToListAsync();
+    }
+}
---
+++ SIM_US_3.Infrastructure/Repositories/Purchasing/PurchaseRepository.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using SIM_US_3.Domain.Models;
+using SIM_US_3.Domain.Models.Interfaces;
+using SIM_US_3.Infrastructure.Common;
+using SIM_US_3.Infrastructure.Data;
+
+namespace SIM_US_3.Infrastructure.Repositories.Purchasing;
+
+public class PurchaseRepository : Repository<Purchase>, IPurchaseRepository
+{
+    public PurchaseRepository(AppDbContext dbContext) : base(dbContext)
+    {
+    }
+
+    public async Task<IEnumerable<Purchase>> GetByUserIdAsync(int userID)
+    {
+        return await _dbset
+            .AsNoTracking()
+            .Where(p => p.UserId == userID)
+            .ToListAsync();
+    }
+
+    public async Task<IEnumerable<Purchase>> GetByPurchaseStatusIdAsync(int statusId)
+    {
+        return await _dbset
+            .AsNoTracking()
+            .Include(p => p.User)
+            .Where(p => p.StatusId == statusId)
+            .ToListAsync();
+    }
+}
